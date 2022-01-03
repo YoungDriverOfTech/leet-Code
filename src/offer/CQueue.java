e629b12ab1fb9754836b5a122e5e9cc4ba87e7a7
liang <xiaoleliang045@gmail.com>
2022-01-03 22:38:06 +0900

    complete first problem

--- src/offer/CQueue.java
+++ src/offer/CQueue.java
@@ -1,21 +1,50 @@
 package offer;
 
+import java.util.LinkedList;
+
 class CQueue {
 
-//    public CQueue() {
-//
-//    }
-//
-//    public void appendTail(int value) {
-//
-//    }
-//
-//    public int deleteHead() {
-//
-//    }
+    private LinkedList<Integer> stackIn;
+    private LinkedList<Integer> stackOut;
 
+    public CQueue() {
+        stackIn = new LinkedList<>();
+        stackOut = new LinkedList<>();
+    }
+
+    public void appendTail(int value) {
+        stackIn.add(value); // notice, do not use push method, it will change insert order and effect final result
+    }
+
+    public int deleteHead() {
+
+        if (!stackOut.isEmpty()) {
+            return stackOut.remove();
+        }
+
+        if (!stackIn.isEmpty()) {
+            stackOut.add(stackIn.remove());
+        }
+
+        return stackOut.isEmpty() ? -1 : stackOut.remove();
+    }
 
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        // case 1
+        CQueue cQueue1 = new CQueue();
+        cQueue1.appendTail(3);
+        System.out.println(cQueue1.deleteHead());
+        System.out.println(cQueue1.deleteHead());
+
+
+        // case 2
+        CQueue cQueue2 = new CQueue();
+        System.out.println(cQueue2.deleteHead());
+        cQueue2.appendTail(5);
+        cQueue2.appendTail(2);
+        System.out.println(cQueue2.deleteHead());
+        System.out.println(cQueue2.deleteHead());
     }
+
 }
