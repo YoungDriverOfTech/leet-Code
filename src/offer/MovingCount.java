a8728ec8e072c9727f3eee0deb7dbc95b074eec8
liangxiaole <xiaoleliang045@gmail.com>
2022-01-16 23:47:10 +0900

    bfs

--- src/offer/MovingCount.java
+++ src/offer/MovingCount.java
@@ -1,5 +1,8 @@
 package offer;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MovingCount {
     public int movingCount(int m, int n, int k) {
         if (k == 0) {
@@ -35,6 +38,40 @@ public class MovingCount {
         dps(i, j - 1, m, n, k, visited);
     }
 
+    // breath-First-Search 深度优先
+    private int bps(int m, int n, int k) {
+        if (k == 0) {
+            return 1;
+        }
+
+        boolean[][] visited = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0});
+
+        int[] dx = new int[]{0, 1};
+        int[] dy = new int[]{1, 0};
+        int result = 0;
+
+        while (!queue.isEmpty()) {
+            int[] indexes = queue.poll();
+            int x = indexes[0];
+            int y = indexes[1];
+
+            if (x < 0 || y < 0 || x >= m || y >= n || visited[x][y] || get(x) + get(y) > k) {
+                continue;
+            }
+            visited[x][y] = true;
+            result++;
+            for (int i = 0; i < 2; i++) {
+                int tx = x + dx[i];
+                int ty = y + dy[i];
+                queue.add(new int[]{tx, ty});
+            }
+        }
+
+        return result;
+    }
+
     private int get(int x) {
         int res = 0;
         while (x != 0) {
@@ -46,5 +83,6 @@ public class MovingCount {
 
     public static void main(String[] args) {
         System.out.println(new MovingCount().movingCount(2, 3, 1));
+        System.out.println(new MovingCount().bps(2, 3, 1));
     }
 }
