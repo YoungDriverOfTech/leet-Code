67283b0c369c5c866d9a8ad2b810f16c2fdab823
liangxiaole <xiaoleliang045@gmail.com>
2022-01-25 19:26:40 +0900

    SingleNumber

--- src/offer/SingleNumber.java
+++ src/offer/SingleNumber.java
@@ -2,6 +2,24 @@ package offer;
 
 public class SingleNumber {
     public int singleNumber(int[] nums) {
-        return 0;
+        int[] k = new int[32]; // 这个树组用来装每个int，每一位是1的个数的和
+        for (int num : nums) {
+            for (int j = 0; j < 32; j++) {
+                k[j] += (num >> j & 1) == 1 ? 1 : 0; // 先判断最右位是不是1，1的话就进行累加。然后右移动1位，继续判断，累加...
+            }
+        }
+
+        int res = 0;
+        for (int i = 31; i >= 0; i--) { // 为什么从31开始，因为上面的循环是从最右位开始累加的。
+            res = res << 1;
+            if (k[i] % 3 == 1) {
+                res = (res ^ 1);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        new SingleNumber().singleNumber(new int[]{3, 4, 3, 3});
     }
 }
