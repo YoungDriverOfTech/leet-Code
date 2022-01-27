575a3f4f0d71e97db6ab2a816d93fdd120853914
liangxiaole <xiaoleliang045@gmail.com>
2022-01-27 12:04:03 +0900

    finish todo

--- src/offer/MyPow.java
+++ src/offer/MyPow.java
@@ -1,7 +1,20 @@
 package offer;
 
 public class MyPow {
+
+    // explanation: https://leetcode-cn.com/problems/powx-n/solution/powx-n-by-leetcode-solution/
     public double myPow(double x, int n) {
-        return 0.0;
+        long N = n;
+        return N >= 0 ? quickMui(x, N) : 1.0 / quickMui(x, -N);
     }
+
+    private double quickMui(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        double result = quickMui(x, n / 2);
+        return n % 2 == 0 ? result * result : result * result * x;
+    }
+
 }
