81ce6af9548e19d398f6e13b90dca6ff3923ac6d
liangxiaole <xiaoleliang045@gmail.com>
2022-01-28 13:37:04 +0900

    update some comments

--- src/offer/BinaryAdd.java
+++ src/offer/BinaryAdd.java
@@ -1,6 +1,7 @@
 package offer;
 
 public class BinaryAdd {
+    // explanation: https://www.bilibili.com/video/BV1F7411j79e?from=search&seid=17789840221403858780&spm_id_from=333.337.0.0
     public int add(int a, int b) {
 
         int sum1 = Integer.MIN_VALUE; // 本位
@@ -8,10 +9,22 @@ public class BinaryAdd {
 
         // 没办法产生进位的时候就退出
         while (sum2 != 0) {
-            sum1 = a ^ b;
-            sum2 = (a & b) << 1;
+/**
+ *         eg： 5 7 相加，换成二进制是  101 111
+            本位             进位
+             101            101
+           + 111           +111
+           = 010           =1010
+            那么最后就是本位和进位在进行相加
+                    1010
+                   + 010
+            上面的数字在进行重复的操作，直到不再产生进位
+*/
 
-            a = sum1;
+            sum1 = a ^ b; // 只用来统计本位想加的情况
+            sum2 = (a & b) << 1; // 只用来统计进位想相加的情况
+
+            a = sum1; // 递归调用
             b = sum2;
         }
 
--- src/offer/ConstructArr.java
+++ src/offer/ConstructArr.java
@@ -1,5 +1,7 @@
 package offer;
 
+import java.util.Arrays;
+
 public class ConstructArr {
 
     // my solution (over time)
@@ -34,17 +36,38 @@ public class ConstructArr {
         int[] result = new int[a.length];
         result[0] = 1;
 
+        System.out.println("first loop start----------");
         for (int i = 1; i < a.length; i++) {
+            System.out.println("----------start " + i + " ----------");
+            System.out.println("result[" + (i - 1) + "] = " + result[i - 1]);
+            System.out.println("a[" + (i - 1) + "] = " + a[i - 1]);
             result[i] = result[i - 1] * a[i - 1]; // result[i]会存储i之前累撑的值
+            System.out.println("result[" + (i) + "] = " + result[i]);
+            System.out.println("----------end " + i + " ----------");
         }
+        System.out.println("first loop end----------");
+
+        System.out.println(Arrays.toString(result));
+
+        System.out.println("second loop start----------");
+
 
         int temp = 1;
         for (int i = a.length - 2; i >= 0; i--) {
+            System.out.println("----------start " + i + " ----------");
+            System.out.println("a[" + (i + 1) + "] = " + a[i + 1]);
+            System.out.println("temp = " + temp);
             temp *= a[i + 1]; // temp 会存储从当前元素到树组末尾的乘积
+            System.out.println("result[" + (i) + "] before = " + result[i]);
             result[i] *= temp;
+            System.out.println("result[" + (i) + "] after = " + result[i]);
+            System.out.println("----------end " + i + " ----------");
         }
-
+        System.out.println("second loop end----------");
         return result;
     }
 
+    public static void main(String[] args) {
+        new ConstructArr().constructArr_1(new int[]{1, 2, 3, 4, 5});
+    }
 }
