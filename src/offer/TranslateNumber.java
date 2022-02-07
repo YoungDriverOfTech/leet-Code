e92fd6726dc3c1ab368810f988f66929b371b9f5
liangxiaole <xiaoleliang045@gmail.com>
2022-02-07 12:11:14 +0900

    update

--- src/offer/TranslateNumber.java
+++ src/offer/TranslateNumber.java
@@ -24,6 +24,33 @@ public class TranslateNumber {
         return dp[s.length() - 1];
     }
 
+    public int translateNum_2(int num) {
+        char[] chars = String.valueOf(num).toCharArray();
+
+        // 注意 这里面的长度不应该num，应该把num转换成字符数组，把字符数组长度放入初始化
+        int[] dp = new int[chars.length]; // store possibly translation count
+        dp[0] = 1;
+
+        for (int i = 1; i < chars.length; i++) {
+            char preCh = chars[i - 1];
+            char curCh = chars[i];
+
+            // fullfill 2 translations condition
+            if ((preCh == '1') || (preCh == '2' && curCh <= '5')) {
+                if (i == 1) {
+                    dp[i] = 2;
+                } else {
+                    dp[i] = dp[i - 1] + dp[i - 2];
+                }
+            } else {
+                dp[i] = dp[i - 1];
+            }
+        }
+
+        // 不应该return dp[num - 1]; 应该return dp[chars.length - 1];
+        return dp[num - 1];
+    }
+
     public static void main(String[] args) {
         System.out.println(new TranslateNumber().translateNum(25));
     }
