2ded2a09cc2b37e1bc84ef71dd27231c9d57603b
liangxiaole <xiaoleliang045@gmail.com>
2022-02-08 19:40:10 +0900

    update

--- src/offer/ConstructArr.java
+++ src/offer/ConstructArr.java
@@ -47,6 +47,30 @@ public class ConstructArr {
         return result;
     }
 
+    // second time
+    public int[] constructArr_2(int[] a) {
+        if (a == null || a.length == 0) {
+            return new int[0];
+        }
+
+        int[] result = new int[a.length];
+        result[0] = 1;
+
+        int tempFirstHalf = 1;
+        for (int i = 1; i < result.length; i++) {
+            tempFirstHalf *= a[i - 1];
+            result[i] = tempFirstHalf;
+        }
+
+        int tempLastHalf = 1;
+        for (int i = result.length - 2; i >= 0; i--) {
+            tempLastHalf *= a[i + 1];
+            result[i] *= tempLastHalf;
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         new ConstructArr().constructArr_1(new int[]{1, 2, 3, 4, 5});
     }
