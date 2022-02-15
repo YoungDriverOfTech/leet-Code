c25754c55989a4b3ef7d18d2296ad9874846003d
liangxiaole <xiaoleliang045@gmail.com>
2022-02-15 22:08:40 +0900

    update

---
+++ src/string/GroupAnagrams.java
@@ -0,0 +1,25 @@
+package string;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    /**
+     * 把每个字符串排序后的结果当作key，排序前的字符串当作value放进map里面，那么map的values就是结果
+     * */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+
+            map.put(key, list);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
