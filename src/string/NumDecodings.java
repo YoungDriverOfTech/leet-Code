1aa154e7be54d2571fc5b21ab8c2501bec9fe2f5
liangxiaole <xiaoleliang045@gmail.com>
2022-02-17 20:41:50 +0900

    update

---
+++ src/string/NumDecodings.java
@@ -0,0 +1,52 @@
+package string;
+
+public class NumDecodings {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0 || s.startsWith("0")) {
+            return 0;
+        }
+
+        // dp代表第i个字符有几种翻译方法，例如：第一个字符就只有1中翻译方法
+        int[] dp = new int[s.length()];
+        dp[0] = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            char preChar = s.charAt(i - 1);
+            char curChar = s.charAt(i);
+
+            // 分为当前字符为0和不为0的情况
+            // 如果当前字符0，切能与前面的字符组成一个合法数字，那么dp[i] = dp[i - 2]; 比如120 or 110
+            // 但是需要注意一种特殊情况，比如10，20 这样只有两位的合法数字，那么翻译方法只有一种，特殊判断即可
+            // 其余的情况都是0， 比如30， 140， 230，因为超过26就不可能被翻译了
+            if (curChar == '0') {
+                if (preChar == '1' || preChar == '2') {
+                    if (i == 1) {
+                        dp[i] = 1;
+                    } else {
+                        dp[i] = dp[i - 2];
+                    }
+                } else {
+                    return 0;
+                }
+            } else {
+                // 当前字符不等于0的话，就是青蛙跳台阶问题了
+                if (preChar == '1' || (preChar == '2' && curChar <= '6')) {
+                    if (i == 1) {
+                        dp[i] = 2;
+                    } else {
+                        dp[i] = dp[i - 1] + dp[i - 2];
+                    }
+                } else {
+                    // 只能被1种方法翻译，那么dp[i] = dp[i - 1]
+                    dp[i] = dp[i - 1];
+                }
+            }
+
+        }
+        return dp[s.length() - 1];
+    }
+
+    public static void main(String[] args) {
+        new NumDecodings().numDecodings("2101");
+    }
+}
--- string.md
+++ string.md
@@ -13,5 +13,5 @@
 | 2022/01/17 | [76. Minimum Window Substring](https://leetcode.com/problems/minimum-window-substring/)                                            | ✅        | Hard       | [Solution](./src/string/MinWindow.java)                |
 | 2022/01/17 | [10. Regular Expression Matching](https://leetcode.com/problems/regular-expression-matching/)                                      | ✅        | Hard       | [Solution](./src/string/isMatch.java)                  |
 | 2022/01/17 | [17. Letter Combinations of a Phone Number](https://leetcode.com/problems/letter-combinations-of-a-phone-number/)                  | ✅        | Medium     | [Solution](./src/string/LetterCombinations.java)       |
-| 2022/01/17 | [91.](https://leetcode.com/problems/longest-palindromic-substring/)                                                                | ✅ Todo   | Medium     | [Solution](./src/string/LongestPalindrome.java)        |
+| 2022/01/17 | [91. Decode Ways](https://leetcode.com/problems/decode-ways/)                                                                      | ✅        | Medium     | [Solution](./src/string/NumDecodings.java)             |
 | 2022/01/14 | [6.](https://leetcode.com/problems/longest-palindromic-substring/)                                                                 | ✅ Todo   | Medium     | [Solution](./src/string/LongestPalindrome.java)        |
