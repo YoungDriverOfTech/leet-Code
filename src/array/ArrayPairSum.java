bd4b62d2266eca517dcb14e5c88240b9e1a8d4a1
liangxiaole <xiaoleliang045@gmail.com>
2022-02-23 21:30:31 +0900

    update

---
+++ src/array/ArrayPairSum.java
@@ -0,0 +1,14 @@
+package array;
+
+import java.util.Arrays;
+
+public class ArrayPairSum {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            result += nums[i];
+        }
+        return result;
+    }
+}
