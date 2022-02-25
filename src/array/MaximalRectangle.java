77f661a1d95e563d984510f0eb53cb198a087e12
liangxiaole <xiaoleliang045@gmail.com>
2022-02-25 22:55:58 +0900

    update

--- src/array/MaximalRectangle.java
+++ src/array/MaximalRectangle.java
@@ -5,8 +5,24 @@ import java.util.Stack;
 public class MaximalRectangle {
     // https://leetcode.wang/leetCode-85-Maximal-Rectangle.html
     public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0) {
+            return 0;
+        }
 
-
+        int[] heights = new int[matrix[0].length];
+        int maxArea = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '1') {
+                    heights[j] += 1;
+                } else {
+                    heights[j] = 0; // 注意，这里不要写成0
+                }
+            }
+            // 计算出最大面积
+            maxArea = Math.max(maxArea, largestRectangleArea_1(heights));
+        }
+        return maxArea;
     }
 
     public int largestRectangleArea_1(int[] heights) {
@@ -37,4 +53,7 @@ public class MaximalRectangle {
         return maxArea;
     }
 
+    public static void main(String[] args) {
+        new MaximalRectangle().maximalRectangle(new char[][]{{'1','0'}});
+    }
 }
