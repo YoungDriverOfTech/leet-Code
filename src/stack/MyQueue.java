4289b8f95f60fb668330778219ccb1e9c29a3186
liangxiaole <xiaoleliang045@gmail.com>
2022-03-17 20:42:52 +0900

    update

--- src/stack/MyQueue.java
+++ src/stack/MyQueue.java
@@ -1,23 +1,43 @@
 package stack;
 
+import java.util.Stack;
+
 public class MyQueue {
-//    public MyQueue() {
-//
-//    }
-//
-//    public void push(int x) {
-//
-//    }
-//
-//    public int pop() {
-//
-//    }
-//
-//    public int peek() {
-//
-//    }
-//
-//    public boolean empty() {
-//
-//    }
+    private Stack<Integer> stackIn;
+    private Stack<Integer> stackOut;
+
+    public MyQueue() {
+        stackIn = new Stack<>();
+        stackOut = new Stack<>();
+    }
+
+    public void push(int x) {
+        stackIn.push(x);
+    }
+
+    public int pop() {
+        if (!stackOut.isEmpty()) {
+            return stackOut.pop();
+        }
+
+        while (!stackIn.isEmpty()) {
+            stackOut.push(stackIn.pop());
+        }
+        return stackOut.pop();
+    }
+
+    public int peek() {
+        if (!stackOut.isEmpty()) {
+            return stackOut.peek();
+        }
+
+        while (!stackIn.isEmpty()) {
+            stackOut.push(stackIn.pop());
+        }
+        return stackOut.peek();
+    }
+
+    public boolean empty() {
+        return stackOut.isEmpty() && stackIn.isEmpty();
+    }
 }
