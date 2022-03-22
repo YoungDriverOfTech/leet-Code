0f81bcaf9af77ef1de647d368d304f38379b5dc5
liangxiaole <xiaoleliang045@gmail.com>
2022-03-22 11:53:57 +0900

    update

--- src/tree/RightSideView.java
+++ src/tree/RightSideView.java
@@ -1,9 +1,38 @@
 package tree;
 
-import java.util.List;
+import java.util.*;
 
 public class RightSideView {
     public List<Integer> rightSideView(TreeNode root) {
-        return null;
+        if (root == null) {
+            return Collections.emptyList();
+        }
+
+        // 层序遍历，把每一层取出来，装进一个list，然后把每一层list的最后一个元素拿出来，让进list即可
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<LinkedList<TreeNode>> resultList = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            LinkedList<TreeNode> list = new LinkedList<>();
+            for (int i = queue.size() - 1; i >= 0; i--) {
+                TreeNode node = queue.poll();
+                list.addFirst(node);
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            resultList.add(list);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (LinkedList<TreeNode> link: resultList) {
+            result.add(link.removeFirst().val);
+        }
+        return result;
     }
 }
