244dac14ad2b8c6c3afc000377ceac0ec6c17552
liangxiaole <xiaoleliang045@gmail.com>
2022-04-09 22:57:00 +0900

    update

---
+++ src/first200/LRUCache.java
@@ -0,0 +1,17 @@
+package first200;
+
+public class LRUCache {
+
+    public LRUCache(int capacity) {
+
+
+    }
+
+    public int get(int key) {
+        return 0;
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
