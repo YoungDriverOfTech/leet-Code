a0509fb0615f5d025076e2962671c3afd7b111c4
liangxiaole <xiaoleliang045@gmail.com>
2022-04-21 22:14:28 +0900

    update

---
+++ src/hot100/ConvertBST.java
@@ -0,0 +1,17 @@
+package hot100;
+
+import dfs.TreeNode;
+
+public class ConvertBST {
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
