7f9ab52560e3037fbea7773c1d4f7bd4ecd3fe1c
liangxiaole <xiaoleliang045@gmail.com>
2022-05-02 15:58:44 +0900

    update

--- src/offer/MedianFinder.java
+++ src/offer/MedianFinder.java
@@ -1,35 +1,56 @@
 package offer;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
 
 public class MedianFinder {
 
     // https://leetcode-cn.com/problems/shu-ju-liu-zhong-de-zhong-wei-shu-lcof/solution/mian-shi-ti-41-shu-ju-liu-zhong-de-zhong-wei-shu-y/
-    private double median;
-    private List<Integer> container;
+    // 使用优先队列，两个优先队列，左边的那个大根堆（栈顶是最大的元素），右边的那个小根堆（栈顶最小的元素）
+    // 比如； left=[1 ,2 ,3, 4, 5] 出队的话是5开始。right=[6, 7, 8, 9, 10]出队的话是6开始
+    // 那么我们让两个队列保持数量相等，切left要多一个，如果数量为偶数，那么中位数就是left和right栈顶的和除以2
+    // 如果是奇数的话，那么就是栈顶的和
+    // https://leetcode-cn.com/problems/find-median-from-data-stream/solution/gong-shui-san-xie-jing-dian-shu-ju-jie-g-pqy8/
+    private PriorityQueue<Integer> left;
+    private PriorityQueue<Integer> right;
 
-    /** initialize your data structure here. */
     public MedianFinder() {
-        this.median = 0;
-        this.container = new ArrayList<>();
+        this.left = new PriorityQueue<>((a, b) -> b - a);   // 放数较小的那一波
+        this.right = new PriorityQueue<>((a, b) -> a - b);  // 放数字较大的那一波
     }
 
     public void addNum(int num) {
-        this.container.add(num);
-        Collections.sort(this.container);
+        int len1 = this.left.size();
+        int len2 = this.right.size();
 
-        int middleIndex = this.container.size() / 2;
-        if (this.container.size() % 2 == 0) {
-            this.median = ((this.container.get(middleIndex) + this.container.get(middleIndex - 1)) / (2 * 1.0)) ;
+        if (len1 == len2) {
+            // 往左边添加的时候，用右head来判断
+            if (right.isEmpty() || num <= right.peek()) {
+                left.add(num);
+            } else {
+                left.add(right.poll());
+                right.add(num);
+            }
+
         } else {
-            this.median = this.container.get(middleIndex);
+            // 往右添加的时候，用左head来判断
+            if (num >= left.peek()) {
+                right.add(num);
+            } else {
+                right.add(left.poll());
+                left.add(num);
+            }
         }
     }
 
     public double findMedian() {
-        return median;
+        int len1 = this.left.size();
+        int len2 = this.right.size();
+
+        if (len1 == len2) {
+            return (left.peek() + right.peek()) / 2.0;
+        } else {
+            return (double) left.peek();
+        }
     }
 
     public static void main(String[] args) {
