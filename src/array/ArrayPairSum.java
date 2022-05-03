df2faa77a8e20b4b7cfa597017bea0b4a475372d
liangxiaole <xiaoleliang045@gmail.com>
2022-05-03 15:10:38 +0900

    update

--- src/array/ArrayPairSum.java
+++ src/array/ArrayPairSum.java
@@ -3,6 +3,9 @@ package array;
 import java.util.Arrays;
 
 public class ArrayPairSum {
+
+    // 题目要求从两个数中挑一个较小的，然后求这些数的和的最大值
+    // 我们只需要排序一次，然后隔一个取一个数字就行了
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
         int result = 0;
