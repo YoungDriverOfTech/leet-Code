f19ca20015bcee03cd96330afa9e66974112e983
liangxiaole <xiaoleliang045@gmail.com>
2022-05-03 15:24:25 +0900

    update

--- src/array/LargestRectangleArea.java
+++ src/array/LargestRectangleArea.java
@@ -38,6 +38,12 @@ public class LargestRectangleArea {
         Stack<Integer> stack = new Stack<>();
         int maxArea = 0;
 
+        // 本题利用单调递增栈，要求出最大面积，那么我们保证放入栈的元素高度是递增的，
+        // 例如我们先放入了1,3,4 此时如果出现了一个2，与栈顶的元素4相比，不是递增，那么就要算出分度分别为4和3的面积是多少，
+        // 然后定义一个变量来维护面积的大小。
+        // 此时遇到2，然后栈弹出4，计算高度为4，宽度为2的索引-4的索引 - 1。
+        // 然后新的栈顶变成了3，那么同上，计算3的面积
+        // 知道遇到了1，又满足了递增，继续入栈
         for (int i = 0; i < heights.length; i++) {
             if (stack.isEmpty()) {
                 stack.add(i);
