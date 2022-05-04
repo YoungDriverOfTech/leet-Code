6219edbe02aee656bdab35e0f73092f048d8e331
liangxiaole <xiaoleliang045@gmail.com>
2022-05-04 16:59:02 +0900

    update

--- src/bs/MinSubArrayLen.java
+++ src/bs/MinSubArrayLen.java
@@ -3,6 +3,9 @@ package bs;
 public class MinSubArrayLen {
 
     // my solution
+    // 遍历数组，然后把每个元素想加，相加完成后，判断和target谁大谁小
+    // 一旦sum >= target, 那么定义一个从0开始的指针，让sum依次减去做指针指向的值，来不停的减少最后result的值
+    // 判断出最终result的值是多少
     public int minSubArrayLen(int target, int[] nums) {
         if (nums.length ==1) {
             if (nums[0] == target) {
