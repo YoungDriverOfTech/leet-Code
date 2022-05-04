f9725294740abdb3380ccbcb673994dbffbf2d71
liangxiaole <xiaoleliang045@gmail.com>
2022-05-04 17:26:42 +0900

    update

--- src/hash/IslandPerimeter.java
+++ src/hash/IslandPerimeter.java
@@ -3,6 +3,7 @@ package hash;
 public class IslandPerimeter {
     // 如果相邻的陆地存在的话，那么就把周长给减上2
     // https://leetcode-cn.com/problems/island-perimeter/comments/
+    // 对每一个单元格的左边和上边进行判断是不是土地，因为这样边界操作会比较方便
     public int islandPerimeter(int[][] grid) {
         if (grid.length == 0 || grid[0].length == 0) {
             return 0;
