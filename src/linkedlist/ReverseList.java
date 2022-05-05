cf76ada3e44269a7494e5b7fe37e639625e22142
liangxiaole <xiaoleliang045@gmail.com>
2022-05-05 11:45:33 +0900

    update

--- src/linkedlist/ReverseList.java
+++ src/linkedlist/ReverseList.java
@@ -20,8 +20,26 @@ public class ReverseList {
         return pre;
     }
 
-    // 递归 todo
+    // 递归
+    public ListNode reverseList_2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
 
+        ListNode p = reverseList(head.next); // 取到的最后一个节点
+
+        // 反转操作
+        // 递归到最后一层，现在p = 5， head是4
+        // 4 -> 5.  反转操作需要  null <- 4 <- 5
+        head.next.next = head; // 5的next指针指向了4
+        head.next = null; // 4的指针指向了空
+
+        // 现在总体的情况变成了
+        // 1 -> 2 -> 3 -> 4 <- 5
+
+        // 然后把p返回给上一轮，处理节点3和4
+        return p;
+    }
 
     // list
     public ListNode reverseList_1(ListNode head) {
