b7059697b7a1c7081866766d723023cec782ea90
liangxiaole <xiaoleliang045@gmail.com>
2022-05-06 17:00:52 +0900

    update

--- src/tree/NumTrees.java
+++ src/tree/NumTrees.java
@@ -28,4 +28,18 @@ public class NumTrees {
         map.put(n, count);
         return count;
     }
+
+    // dp
+    // https://leetcode-cn.com/problems/unique-binary-search-trees/solution/hua-jie-suan-fa-96-bu-tong-de-er-cha-sou-suo-shu-b/
+    public int numTrees_1(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i < n + 1; i++)
+            for(int j = 1; j < i + 1; j++)
+                dp[i] += dp[j-1] * dp[i-j];
+
+        return dp[n];
+    }
 }
