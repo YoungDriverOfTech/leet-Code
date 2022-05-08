b523e835c2f3f4431b0f9f7451d9b095b1120498
liangxiaole <xiaoleliang045@gmail.com>
2022-05-08 15:35:39 +0900

    update

--- src/tree/IsSymmetric.java
+++ src/tree/IsSymmetric.java
@@ -1,6 +1,11 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class IsSymmetric {
+
+    // 递归
     public boolean isSymmetric(TreeNode root) {
         if (root == null) {
             return true;
@@ -30,5 +35,33 @@ public class IsSymmetric {
 
         return isSymmetric(node1.left, node2.right) &&
                 isSymmetric(node1.right, node2.left);
+    }
+
+    // 迭代
+    public boolean isSymmetric_1(TreeNode root) {
+        return check(root, root);
+    }
+
+    public boolean check(TreeNode u, TreeNode v) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(u);
+        q.offer(v);
+        while (!q.isEmpty()) {
+            u = q.poll();
+            v = q.poll();
+            if (u == null && v == null) {
+                continue;
+            }
+            if ((u == null || v == null) || (u.val != v.val)) {
+                return false;
+            }
+
+            q.offer(u.left);
+            q.offer(v.right);
+
+            q.offer(u.right);
+            q.offer(v.left);
+        }
+        return true;
     }
 }
