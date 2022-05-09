8cf18b076579d96538de440a3375ef9dc89ea3d3
liangxiaole <xiaoleliang045@gmail.com>
2022-05-09 14:10:05 +0900

    update

--- src/top200/CountAndSay.java
+++ src/top200/CountAndSay.java
@@ -4,29 +4,25 @@ public class CountAndSay {
 
     // https://leetcode-cn.com/problems/count-and-say/solution/by-a_tao-9yjh/
     public String countAndSay(int n) {
+        // 让索引从1开始，方便计算
         String s = "1";
 
         for (int i = 2; i <= n; i++) {
             StringBuilder sb = new StringBuilder();
-            int len = s.length();   // 字符串的商都
-            int count = 1;  // 每个字符出现的次数
+            int start = 0;
+            int pos = 0;
 
-            for (int j = 1; j < len; j++) {
-                // 如果字符和前一个字符不同，那么就需要追加道sb里面
-                if (s.charAt(j - 1) != s.charAt(j)) {
-                    sb.append(count).append(s.charAt(j - 1));
-
-                    // 一个字符追加完毕，要初始化count次数
-                    count = 1;
-                } else {
-                    count++;
+            while (pos < s.length()) {
+                while (pos < s.length() && s.charAt(start) == s.charAt(pos)) {
+                    pos++;
                 }
+                sb.append(pos - start).append(s.charAt(start));
+
+                // 遍历下一个字符
+                start = pos;
             }
-
-            // 添加长度和字符
-            s = sb.append(count).append(s.charAt(len - 1)).toString();
+            s = sb.toString();
         }
-
         return s;
     }
 
