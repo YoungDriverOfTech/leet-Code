af7d41ff0d4204a4e96ddd5c495ea47cde3389dc
liangxiaole <xiaoleliang045@gmail.com>
2022-05-10 11:19:28 +0900

    update

--- src/top200/MissingNumber.java
+++ src/top200/MissingNumber.java
@@ -8,14 +8,16 @@ public class MissingNumber {
 
     // 位运算
     // https://leetcode-cn.com/problems/missing-number/solution/diu-shi-de-shu-zi-by-leetcode-solution-naow/
+    // 因为根据异或运算，两个形同的数如果进行异或运算，那么结果就是0，那么我们给定一个0，进行两次遍历然后进行异或操作，那么只出现过一次的数
+    // 就会作为最后的结果出现
     public int missingNumber(int[] nums) {
         int result = 0;
         for (int i = 0; i < nums.length; i++) {
             result ^= nums[i];
         }
 
-        for (int i = 0; i <= nums.length; i++) {
-            result ^= i;
+        for (int i = 0; i <= nums.length; i++) {    // 注意第二遍有等号
+            result ^= i;    // 给索引异或
         }
 
         return result;
