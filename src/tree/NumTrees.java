46acdaa78a6f6e4bd54cb9fee37145f3654da4da
liangxiaole <xiaoleliang045@gmail.com>
2022-05-15 17:38:09 +0900

    update

--- src/tree/NumTrees.java
+++ src/tree/NumTrees.java
@@ -31,14 +31,17 @@ public class NumTrees {
 
     // dp
     // https://leetcode-cn.com/problems/unique-binary-search-trees/solution/hua-jie-suan-fa-96-bu-tong-de-er-cha-sou-suo-shu-b/
+    // https://leetcode.cn/problems/unique-binary-search-trees/solution/c-by-xuezhaplus-f5mt/
     public int numTrees_1(int n) {
         int[] dp = new int[n+1];
         dp[0] = 1;
         dp[1] = 1;
 
-        for(int i = 2; i < n + 1; i++)
-            for(int j = 1; j < i + 1; j++)
+        for(int i = 2; i < n + 1; i++) {
+            for(int j = 1; j < i + 1; j++) {
                 dp[i] += dp[j-1] * dp[i-j];
+            }
+        }
 
         return dp[n];
     }
