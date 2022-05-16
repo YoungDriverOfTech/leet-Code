a5d226764b88c78d70ca13293a3604583dfd2bc5
liangxiaole <xiaoleliang045@gmail.com>
2022-05-16 11:22:02 +0900

    update

--- src/stack/RemoveDuplicateLetters.java
+++ src/stack/RemoveDuplicateLetters.java
@@ -17,18 +17,26 @@ public class RemoveDuplicateLetters {
         // 用栈来统计每个字符，如果该字符在栈中出现过，那么就进行下一个字符的统计，如果没出现过
         // 那么就要判断当前字符和栈顶的字符的大小，如果当前字符小于栈顶的字符，那么需要把栈顶的字符pop出现、
         // 来保证最小的字典顺序，如果栈顶的字符只剩下最后一个了，那就不要pop了，因为会把必要的字符删除掉
+
+        // 以"cbacdcbc"为例子
         Stack<Character> stack = new Stack<>();
         for (char ch : chars) {
             map.put(ch, map.get(ch) - 1);   // 注意，每遍历一个字符，就要把数量减去1
+
+            // 有重复元素那么就跳过
             if (stack.contains(ch)) {
                 continue;
             }
 
+            // 插入之前，和之前的元素比较一下大小
+            // 如果字典序比前面的小，pop 前面的元素
             while (!stack.isEmpty() && stack.peek() > ch) {
+
+                // 若之后不存在栈顶元素了，则停止 pop
                 if (map.get(stack.peek()) == 0) {
                     break;
                 }
-
+                // 若之后还有，则可以 pop
                 stack.pop();
             }
 
