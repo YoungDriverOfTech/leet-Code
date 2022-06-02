1b3bfb66cd56dc83dfe9ff3d1d8a40cadbc7f79f
liangxiaole <xiaoleliang045@gmail.com>
2022-06-02 15:27:35 +0900

    update

--- src/bs/FindDuplicate.java
+++ src/bs/FindDuplicate.java
@@ -30,6 +30,28 @@ public class FindDuplicate {
         return low;
     }
 
+    public int findDuplicate_1(int[] nums) {
+        /**
+         快慢指针思想, fast 和 slow 是指针, nums[slow] 表示取指针对应的元素
+         注意 nums 数组中的数字都是在 1 到 n 之间的(在数组中进行游走不会越界),
+         因为有重复数字的出现, 所以这个游走必然是成环的, 环的入口就是重复的元素,
+         即按照寻找链表环入口的思路来做
+         **/
+        int fast = 0, slow = 0;
+        while(true) {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+            if(slow == fast) {
+                fast = 0;
+                while(nums[slow] != nums[fast]) {
+                    fast = nums[fast];
+                    slow = nums[slow];
+                }
+                return nums[slow];
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(new FindDuplicate().findDuplicate(new int[]{7,9,7,4,2,8,7,7,1,5}));
     }
