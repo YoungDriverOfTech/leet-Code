2afd3e5b3a0113b3e2240aa2c8ff5f565a853a9a
liangxiaole <xiaoleliang045@gmail.com>
2022-06-14 20:35:55 +0900

    update

--- src/top200/Jump.java
+++ src/top200/Jump.java
@@ -1,6 +1,8 @@
 package top200;
 
 public class Jump {
+
+    // time: O(n) / space: O(1)
     public int jump(int[] nums) {
         int maxPosition = 0;    //  遍历每个元素可以到达的最远距离位置
         int step = 0;   // 跳跃步数
@@ -14,6 +16,26 @@ public class Jump {
             if (i == end) {
                 end = maxPosition;
                 step++;
+            }
+        }
+
+        return step;
+    }
+
+    // time: O(n * n) / space: O(1)
+    // 先找到最后一个索引，然后从左往右数，看哪个元素能直接到达最后一个索引，如果到了，那么step++，记当前索引是p1
+    // 然后把再从左往右找，看哪个索引能直接p1，一直到开头的元素
+    public int jump_1(int[] nums) {
+        int position = nums.length - 1;
+        int step = 0;
+        while (position > 0) {
+
+            for (int i = 0; i < position; i++) {
+                int maxReach = i + nums[i];
+                if (maxReach >= position) {
+                    step++;
+                    position = i;
+                }
             }
         }
 
