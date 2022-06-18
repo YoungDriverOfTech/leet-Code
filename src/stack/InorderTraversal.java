a7090c2c0f9add08820571f07e517cf1ea7ee86d
liangxiaole <xiaoleliang045@gmail.com>
2022-06-18 23:45:39 +0900

    update

--- src/stack/InorderTraversal.java
+++ src/stack/InorderTraversal.java
@@ -2,6 +2,7 @@ package stack;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class InorderTraversal {
     public List<Integer> inorderTraversal(TreeNode root) {
@@ -22,5 +23,22 @@ public class InorderTraversal {
         dfs(node.left, list);
         list.add(node.val);
         dfs(node.right, list);
+    }
+
+    public List<Integer> inorderTraversal_1(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+
+        return res;
     }
 }
