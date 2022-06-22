8f13203750d68effac74ce3501ec11fe7e6180b7
liangxiaole <xiaoleliang045@gmail.com>
2022-06-22 21:37:53 +0900

    update

--- src/doublePointor/RemoveElement.java
+++ src/doublePointor/RemoveElement.java
@@ -3,19 +3,18 @@ package doublePointor;
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
         // 题目并没有要求排序，所以不用排序也行
-        int left = 0;   // 最终结果的索引
-        int right = nums.length;    // 遍历结果的索引
+        int left = 0;
+        int right = nums.length - 1;
 
-        // 如果左边的索引值等于val，那么就把右边的值贴到左边，然后右边索引-1，再继续判断left索引的值。
-        // 如果不等于的话，那么就把left向右移
-        while (left < right) {
+        while (left <= right) { // 这里要求等于，因为等于的时候，也有可能这个元素是有效值，所以需要再计算一次
             if (nums[left] == val) {
-                nums[left] = nums[right - 1];
+                nums[left] = nums[right];
                 right--;
             } else {
                 left++;
             }
         }
+
         return left;
     }
 }
