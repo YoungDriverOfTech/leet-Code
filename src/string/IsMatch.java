e88271c5c2da8b4d0672d1b38d97c139d3624f27
liangxiaole <xiaoleliang045@gmail.com>
2022-07-17 20:14:33 +0900

    update

--- src/string/IsMatch.java
+++ src/string/IsMatch.java
@@ -7,7 +7,6 @@ public class IsMatch {
         // last letter: *
         //   0 time:   dp[i][j] = dp[i][j - 2];
         //  >1 time:   dp[i][j] = dp[i - 1][j];
-
         int m = s.length();
         int n = p.length();
         boolean[][] dp = new boolean[m + 1][n + 1]; // plus 1 is for preventing  m/n is zero
@@ -24,15 +23,12 @@ public class IsMatch {
                         if (i > 0 && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.')) {
                             dp[i][j] = dp[i - 1][j - 1];
                         }
-
                     } else {
-
                         // if last letter of p is *, then we should consider how many times * and previous letter show
                         // for 0 time
                         if (j >= 2) {
                             dp[i][j] |= dp[i][j - 2];
                         }
-
 
                         // for >0 times
                         if ((i >= 1 && j >= 2) && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') ) {
@@ -42,7 +38,6 @@ public class IsMatch {
                 }
             }
         }
-
         return dp[m][n];
     }
 
